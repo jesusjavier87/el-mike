39c368e038a2a75180cabeaaaca2e89a42b642e7
jesusjavier87 <jesusjavierpachecoortiz@gmail.com>
2021-12-06 22:26:04 -0600

    este enviar los datos entre pages

--- src/views/pages/Alerts.vue
+++ src/views/pages/Alerts.vue
@@ -1,27 +1,68 @@
 <template>
-  <v-container fluid class="down-top-padding">
-   <!--esta es la page es de agenda(jesus)-->
-   <v-row>
-      <v-col cols="12" sm="12">
-        <BaseCard heading="Lista de Agenda">
-            <TableSimpleFixHeight>
-
-            </TableSimpleFixHeight>
-        </BaseCard>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-container fluid class="down-top-padding">
+       <div>
+        <v-list-item-subtitle class="text-wrap">
+        </v-list-item-subtitle>
+        <div class="mt-4">
+            <v-simple-table height="500px">
+                <template v-slot:default>
+                <thead>
+                    <tr>
+                    <th class="text-left">id</th>
+                    <th class="text-left">fecha de inicio</th>
+                    <th class="text-left">fecha final</th>
+                    <th class="text-left">hora</th>
+                    <th class="text-left">acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in data" :key="item.id">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.dtDiaAgenda }}</td>
+                    <td>{{ item.dtFechaCuidar }}</td>
+                    <td>{{ item.hora }}</td>
+                    <td>
+                      <v-btn depressed color="primary" @click="verdeta(item.id)">Ver</v-btn>
+                    </td>
+                    </tr>
+                </tbody>
+                </template>
+            </v-simple-table>
+        </div>
+    </div>
+    </v-container>
 </template>
+ 
+ 
+ 
 
-<script>
+ <script>
+import axios from "axios";
 export default {
-  name: "TableSimple",
-
+  name: "Current",
   data: () => ({
+    items: ["albums", "todos", "posts"],
+    selected: "",
+    data:[],
+    id:0,
   }),
-  components: {
-      TableSimpleFixHeight: () => import('@/components/vuetifyComponents/tables-simple/TableSimpleFixHeight'),
-  }
-};
 
+  methods: {
+    getData() {
+      axios
+        .get("https://apipet18301044.herokuapp.com/agendar" + this.selected ,{dataType: "json"})
+        .then((response) => { this.data = response.data;
+        })
+        .catch((err) => alert(err));
+    },
+    verdeta(id){
+                this.$router.push(`/pages/deta-agenda/?id=${id}`);
+            },
+  },
+  mounted() {
+    this.getData();
+  }
+
+};
 </script>
+
--- src/views/pages/DetaAgenda.vue
+++ src/views/pages/DetaAgenda.vue
@@ -1,11 +1,62 @@
 <template>
-    <v-container>
-
-    </v-container>
+  <v-container fluid class="down-top-padding">
+     <!--esta es la page es de mi perfil(jesus)-->
+      <v-col>
+        <v-card>
+          <v-card-text>
+            <h3 class="title blue-grey--text text--darken-2 font-weight-regular">Detalles Agenda</h3>
+            <h6 class="subtitle-2 font-weight-light">Esta seccion es ver a detalle una agenda</h6>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-text>
+              <v-text-field
+              v-model="id"
+              label="id:"
+              filled
+              disabled = "false"
+              background-color="transparent"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="dtDiaAgenda"
+              label="dtDiaAgenda:"
+              filled
+               disabled = "false"
+              background-color="transparent"
+            ></v-text-field>
+             <v-text-field
+              v-model="dtFechaCuidar"
+              label="dtFechaCuidar:"
+              filled
+               disabled = "false"
+              background-color="transparent"
+            ></v-text-field>
+            <v-text-field
+              v-model="hora"
+              label="hora:"
+              filled
+               disabled = "false"
+              background-color="transparent"
+            ></v-text-field>
+             <v-text-field
+              v-model="strDescripcion"
+              label="strDescripcion:"
+              filled
+               disabled = "false"
+              background-color="transparent"
+            ></v-text-field>
+          </v-card-text>
+        </v-card>
+      </v-col>
+  </v-container>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+          id: this.$route.query.id,
+        };
+      },
 }
 </script>
