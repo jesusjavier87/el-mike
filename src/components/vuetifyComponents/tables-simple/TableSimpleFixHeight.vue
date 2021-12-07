a5efbcf5f58da2fe077c9bc579a795651664d258
jesusjavier87 <jesusjavierpachecoortiz@gmail.com>
2021-12-06 19:52:53 -0600

    la vida apesta

--- src/components/vuetifyComponents/tables-simple/TableSimpleFixHeight.vue
+++ src/components/vuetifyComponents/tables-simple/TableSimpleFixHeight.vue
@@ -2,27 +2,33 @@
     <!-- ----------------------------------------------------------------------------- -->
     <!-- TableSimpleFixHeight -->
     <!-- ----------------------------------------------------------------------------- -->
-     <div>
+    <div>
         <v-list-item-subtitle class="text-wrap">
         </v-list-item-subtitle>
         <div class="mt-4">
             <v-simple-table height="300px">
-
                 <template v-slot:default>
                 <thead>
                     <tr>
-                    <th class="text-left">Id</th>
-                    <th class="text-left">Name</th>
+                    <th class="text-left">id</th>
+                    <th class="text-left">fecha de inicio</th>
+                    <th class="text-left">fecha final</th>
+                    <th class="text-left">hora</th>
+                    <th class="text-left">acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>2</td>
+                    <tr v-for="item in data" :key="item.id">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.dtDiaAgenda }}</td>
+                    <td>{{ item.dtFechaCuidar }}</td>
+                    <td>{{ item.hora }}</td>
+                    <td>
+                      <v-btn depressed color="primary" >Primary</v-btn>
+                    </td>
                     </tr>
                 </tbody>
                 </template>
-
             </v-simple-table>
         </div>
     </div>
@@ -31,34 +37,29 @@
  
  
 
-<script>
+ <script>
 import axios from "axios";
 export default {
   name: "Current",
   data: () => ({
     items: ["albums", "todos", "posts"],
     selected: "",
-
-    headers: [
-      { text: "ID", value: "id" },
-      { text: "firstname", value: "firstname" },
-    ],
     data:[],
   }),
 
   methods: {
     getData() {
       axios
-        .get("https://apipet18301044.herokuapp.com/users" + this.selected ,{dataType: "json"})
+        .get("https://apipet18301044.herokuapp.com/agendar" + this.selected ,{dataType: "json"})
         .then((response) => { this.data = response.data;})
         .catch((err) => alert(err));
     },
   },
-
   mounted() {
     this.getData();
   }
 
 };
 </script>
- 
+
+
