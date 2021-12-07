79c1c890dc499b27073c81f0ef19215d5b7e2052
jesusjavier87 <jesusjavierpachecoortiz@gmail.com>
2021-12-07 11:33:11 -0600

    pestas mike

--- src/views/pages/Profile.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container fluid class="down-top-padding">
-     <!--esta es la page es de terminos y condiciones(mike)-->
-  </v-container>
-</template>
-
-<script>
-
-</script>
--- src/views/pages/TableSimple.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container fluid class="down-top-padding">
- <!--esta es la page es de manual(mike)-->
-  </v-container>
-</template>
-
-<script>
-
-</script>
